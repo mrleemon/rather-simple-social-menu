a42563abde8af16cf81053c865e73b8d5fb94fc8
leemon <tacrapo@gmail.com>
2020-01-04 13:01:14 +0100

    Add missing spacing

--- rather-simple-social-menu-widget.php
+++ rather-simple-social-menu-widget.php
@@ -16,7 +16,7 @@ class Rather_Simple_Social_Menu_Widget extends WP_Widget {
 	 *
 	 */
 	public function __construct() {
-		load_plugin_textdomain( 'rather-simple-social-menu-widget', '', dirname(plugin_basename( __FILE__ )) . '/languages/' );
+		load_plugin_textdomain( 'rather-simple-social-menu-widget', '', dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 		$widget_ops = array(
 			'description' => __( 'Add a custom social menu to your sidebar.', 'rather-simple-social-menu-widget' ),
 			'customize_selective_refresh' => true,
