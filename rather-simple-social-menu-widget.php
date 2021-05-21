1eb3578b7f8ce2fa4ce589d5834085550a92e87b
leemon <tacrapo@gmail.com>
2021-05-21 11:38:40 +0200

    Update plugin header

--- rather-simple-social-menu-widget.php
+++ rather-simple-social-menu-widget.php
@@ -8,6 +8,20 @@ Author: Oscar Ciutat
 Author URI: http://oscarciutat.com/code/
 Description: A really simple social menu widget
 Text Domain: rather-simple-social-menu-widget
+License: GPLv2 or later
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2, as 
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 class Rather_Simple_Social_Menu_Widget extends WP_Widget {
